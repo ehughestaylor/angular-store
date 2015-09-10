74b142822258e753515abcf6058c321151a2312a
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-09-10 17:30:25 -0400

    created static page displaying information about two phones as well as a counter for how many phones there are in the list

---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ehughestaylorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ehughestaylorApp
+ */
+angular.module('ehughestaylorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
